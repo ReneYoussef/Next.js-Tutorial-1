de8b4f5b4eb3acb697297aa66a29e8fae0aea344
Rene A Youssef <Reneabdo10@gmail.com>
2025-05-26 15:58:53 +0300

    first next js tutorial : understanding title MetaData

--- src/app/(site)/blog/page.tsx
+++ src/app/(site)/blog/page.tsx
@@ -1,3 +1,11 @@
+import { Metadata } from "next"
+
+export const metadata: Metadata = {
+    title: {
+         absolute: "Blog",
+    }
+}//static metadata for the about page
+
 export default function blog(){
     return <h1>blog page</h1>
 }
