e2813c6f1a4689f215575050edc0d65e6385cea1
Rene A Youssef <Reneabdo10@gmail.com>
2025-05-26 16:18:46 +0300

    first next js tutorial : understanding linking Componenets

--- src/app/(marketing)/product/page.tsx
+++ src/app/(marketing)/product/page.tsx
@@ -1,10 +1,25 @@
+import Link from "next/link"
+
 export default function productList(){
+
+    const productId=1000
     return (
     <>
+
+    <Link href="/">Home</Link>
     <h1>product List</h1>
-    <h3>product 1</h3>
-    <h3>product 2</h3>
-    <h3>product 3</h3>
+    <h3>
+        <Link href="/product/1">product 1</Link>
+    </h3>
+    <h3>
+        <Link href="/product/2">product 2</Link>
+    </h3>
+    <h3>
+        <Link href="/product/3" replace>product 3</Link>
+    </h3>
+    <h3>
+    <Link href={`/product/${productId}`}>product {productId}</Link>
+</h3>
     </>
 
     )
--- src/app/(site)/page.tsx
+++ src/app/(site)/page.tsx
@@ -1,3 +1,12 @@
-export default function home(){
-    return <h1>welcome home</h1>
+import Link from "next/link";
+
+export default function home() {
+  return (
+    <>
+   
+      <h1>welcome home</h1>
+      <Link href="/blog">Blog</Link>
+    <Link href="/product">Product</Link>
+    </>
+  );
 }
